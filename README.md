f517e5d233d9b069d99285b7287a39be13cde6a3
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2020-02-03 01:50:18 +0100

    Add method comments

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # mars-robot-task
 A robot lands on Mars, which happens to be a cartesian grid; assuming that we hand the robot these instructions, such as LFFFRFFFRRFFF, where "L" is a "turn 90 degrees left", "R" is a "turn 90 degrees right", and "F" is "go forward one space, please write control code for the robot such that it ends up at the appropriate-and-correct destination
 
-# Starting info
+# Basic info
 Robot requires to have known facing direction when starting. These can be NORTH, EAST, SOUTH or WEST.
 
 Robot always starts at [0,0] coordination.
 
 Feel free to extend the provided robot with custom implementation and add extra behaviour.
+You can easily train the robot some basic tricks like turn 360 degrees, turn 180 degrees, move in a square shape, etc.
 
 ### Navigating the robot
 Robot supports 3 commands:
@@ -24,3 +25,10 @@ execute the command. If it isn't it will report an error.
 
 # Tests
 Use the standard `test` task from gradle to run all tests.
+
+# Tech stack
+Tested on:
+
+* OpenJDK 11 (should work on earlier versions also)
+* jUnit 5
+* Gradle 6.0.1
--- src/main/java/com/kayak/marsrobot/model/Robot.java
+++ src/main/java/com/kayak/marsrobot/model/Robot.java
@@ -10,6 +10,11 @@ public class Robot implements RobotInterface {
     private FacingDirection currentFacingDirection;
     private Coordinate coordinate;
 
+    /**
+     * Initial constructor. Requires initial facing direction which can be any of the {@link FacingDirection} values
+     *
+     * @param initialFacingDirection
+     */
     public Robot(FacingDirection initialFacingDirection) {
         if (initialFacingDirection == null) {
             throw new IllegalArgumentException("Invalid facing direction");
@@ -18,6 +23,12 @@ public class Robot implements RobotInterface {
         this.coordinate = new Coordinate(0, 0);
     }
 
+    /**
+     * Move the robot giving it one or more characters combined into a single command. All valid characters are listd
+     * in the {@link MovementInstruction}
+     *
+     * @param commands
+     */
     @Override
     public void move(String commands) {
         if (commands == null || commands.isBlank()) {
@@ -40,38 +51,62 @@ public class Robot implements RobotInterface {
         }
     }
 
-
+    /**
+     * Move the robot 1 step forward
+     */
     protected void moveForward() {
         if (currentFacingDirection == FacingDirection.NORTH) {
             coordinate.setY(coordinate.getY() + 1);
+            return;
         }
 
         if (currentFacingDirection == FacingDirection.EAST) {
             coordinate.setX(coordinate.getX() + 1);
+            return;
         }
 
         if (currentFacingDirection == FacingDirection.SOUTH) {
             coordinate.setY(coordinate.getY() - 1);
+            return;
         }
 
         if (currentFacingDirection == FacingDirection.WEST) {
             coordinate.setX(coordinate.getX() - 1);
+            return;
         }
+
+        throw new RuntimeException("Unsupported current facing direction!");
     }
 
+    /**
+     * Rotate the robot 90 degrees to the left
+     */
     protected void turnLeft() {
         currentFacingDirection = FacingDirectionResolver.changeFacingDirection(currentFacingDirection, MovementInstruction.TURN_LEFT);
     }
 
+    /**
+     * Rotate the robot 90 degrees to the right
+     */
     protected void turnRight() {
         currentFacingDirection = FacingDirectionResolver.changeFacingDirection(currentFacingDirection, MovementInstruction.TURN_RIGHT);
     }
 
+    /**
+     * Get current coordinates
+     *
+     * @return
+     */
     @Override
     public Coordinate getCoordinate() {
         return coordinate;
     }
 
+    /**
+     * Get current facing direction
+     *
+     * @return
+     */
     @Override
     public FacingDirection getCurrentFacingDirection() {
         return currentFacingDirection;
